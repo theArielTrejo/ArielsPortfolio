598d258350309a59fa15b95ec40658748a6492bf
Ariel T. <arieltrejo0804@gmail.com>
2026-04-30 13:58:33 -0500

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./aboutme";
 import Work from "./work";
 import Publication from "./publication";
 import Hobbies from "./hobbies";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
